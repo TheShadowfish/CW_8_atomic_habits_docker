000acba14d11b76fdb389f3e6a9b3c15a8648805
TheShadowfish <shadowfish@yandex.ru>
2024-08-01 11:56:11 +0300

    create User, tested

--- users/tests.py
+++ users/tests.py
@@ -39,15 +39,75 @@ class UserTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(data.get("email"), "test2@test.com")
-        self.assertEqual(data.get("password"), "testpassword")
+        # self.assertEqual(data.get("password"), "testpassword")
         self.assertEqual(data.get("tg_chat_id"), "1567728836")
         self.assertEqual(data.get("is_superuser"), False)
+
+    def test_create_user_no_tg_chat_id(self):
+        """ Тестирование создания пользователя """
+
+        url = reverse("users:register")
+        data = {
+            "email": "test2@test.com",
+            "password": "testpassword",
+            "is_superuser": "False"
+        }
+
+        response = self.client.post(url, data=data)
+        data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
 
     def test_list_user(self):
         """ Тестирование вывода всех пользователей """
 
         response = self.client.get(reverse('users:users_list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data=response.json()
+
+        last_login_text = str(self.user.last_login)
+        last_login = last_login_text[0:10] + 'T' + last_login_text[11:26] + 'Z'
+        date_joined_text = str(self.user.date_joined)
+        date_joined = date_joined_text[0:10] + 'T' + date_joined_text[11:26] + 'Z'
+        # print(f"created {created}")
+        # print(f"updated {updated}")
+
+        result = {
+        "email": self.user.email,
+        "first_name": "",
+        "last_name": "",
+        "tg_nick": None,
+        "tg_chat_id": self.user.tg_chat_id,
+        "last_login": last_login,
+        "avatar": None,
+        "date_joined": date_joined,
+        "is_superuser": True,
+        "is_staff": False,
+        "is_active": True
+    }
+        # print(f"created_at:{self.course.created_at} updated_at:{self.course.updated_at}")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, result)
+
+
+
+        """[
+    {
+        "email": "test@test.com",
+        "first_name": "",
+        "last_name": "",
+        "tg_nick": null,
+        "tg_chat_id": "1567728836",
+        "last_login": "2024-08-01T08:45:11.811158Z",
+        "avatar": null,
+        "date_joined": "2024-08-01T08:24:53.606463Z",
+        "is_superuser": true,
+        "is_staff": false,
+        "is_active": true
+    }
+]"""
 
     def test_user_retrieve(self):
         """ Тестирование просмотра одного пользователя """
