9776f811567663ce3373111d61abc12352d11260
TheShadowfish <shadowfish@yandex.ru>
2024-08-05 14:34:10 +0300

    validators

--- users/serializers.py
+++ users/serializers.py
@@ -9,6 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        read_only_fields = ['password']
 
 
 class UserLimitedSerializer(serializers.ModelSerializer):
